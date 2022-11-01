85c8164bc8f1293e91b45696354c750b5245fc20
punkmakier <69425047+punkmakier@users.noreply.github.com>
2022-11-01 22:34:48 +0800

    first commit

--- Views/footer.php
+++
@@ -1,3 +0,0 @@
-<section id="footer">
-    <p>Ⓒ Copyright 2022 | M.A.C e-Portfolio | All right reserved</p>
-</section>
--- Views/letswork.php
+++
@@ -1,8 +0,0 @@
-<section id="hireme">
-    <div class="inner-hireme">
-        <h2>Let's Work Together</h2>
-        <p>I’m available for freelance projects. Hire me and get your project done.</p>
-        <br>
-        <a href="#contact">HIRE ME</a>
-    </div>
-</section>
