d71cb07de1a9a84ef129fd807e2158020e072ca2
punkmakier <69425047+punkmakier@users.noreply.github.com>
2023-04-08 20:37:06 +0800

    updated eportfolio

--- inc/mailersystem.php
+++ inc/mailersystem.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['submit'])){
+if(isset($_POST['submit']) == "submit"){
 // lootytkdcoecbquz
 // 0932061675mark
 
@@ -13,17 +13,18 @@ if(isset($_POST['submit'])){
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'klintoiyas@gmail.com';                 // SMTP username
-    $mail->Password = 'lootytkdcoecbquz';                           // SMTP password
+    $mail->Password = 'moahfsqrxwtfnvow';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
-    $mail->setFrom('klintoiyas@gmail.com', 'My E-Portfolio Mailer');
-    $mail->addAddress($_POST['sendEmail'], 'Sender');     // Add a recipient
+
+    $mail->setFrom("klintoiyas@gmail.com", 'My E-PORTFOLIO MAILER');
+    $mail->addAddress("carbamark@gmail.com");     // Add a recipient
 
     $mail->isHTML(true);                                  // Set email format to HTML
 
-    $mail->Subject = 'New Entry';
-    $mail->Body    = $_POST['message'];
+    $mail->Subject = 'PRIORITY: NEW INQUIRY';
+    $mail->Body    = "Sender: ".$_POST['sendEmail']."<br>Complete Name: ".$_POST['sendName']."<br><br>Message: ".$_POST['message'];
 
     if(!$mail->send()) {
         echo 'Failed';
